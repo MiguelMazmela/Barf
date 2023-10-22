455cc96e280952c5d62a0a4845181a1d80aa1af4
MiguelMazmela <miguelmazmela@gmail.com>
2023-10-22 11:13:19 -0500

    correccion 22-10-2023
    corregido el problema de cambio de maquina, soporta otros sistemas operativos

--- src/Clases/conecta.java
+++ src/Clases/conecta.java
@@ -5,6 +5,7 @@
  */
 package Clases;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -12,40 +13,100 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
-
 /**
  *
  * @author PORTATIL
  */
 public final class conecta {
 
+    private String sSistemaOperativo;
+    private String CaminoCompletoBD;
+    private boolean conectado = false;
+    private String usuario;
+    private Boolean UsuarioValido;
+    private boolean edicion = false;
+
     public conecta() {
-        
+
         this.conectar();
-        
+
     }
-    
+
     public Connection conectar() {
         Connection con = null;
-        
+        this.sSistemaOperativo = System.getProperty("os.name");
+        String sFichero = "mi_base.db";
+        String sDirectorio = "db";
+
+        String sPath = getCurrentDir() + File.separator + sDirectorio + File.separator + sFichero;
+        CaminoCompletoBD = sPath;
+
         try {
-            Class.forName("org.sqlite.JDBC"); 
-            String url = "/home/miguel/Mio/Java/barf/src/db/mi_base.db";
-            con = DriverManager.getConnection("jdbc:sqlite:"+url);
+            Class.forName("org.sqlite.JDBC");
+//            String url = "/home/miguel/Mio/Java/barf/src/db/mi_base.db";
+            con = DriverManager.getConnection("jdbc:sqlite:" + sPath);
             System.out.print("conecto");
         } catch (SQLException ex) {
             System.out.print(ex);
-            JOptionPane.showMessageDialog(null,"No se conecto");
-            
+            JOptionPane.showMessageDialog(null, "No se conecto");
+
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(conecta.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         return con;
-        
+
     }
-    
-    public void cierra(){
+
+    public String getsSistemaOperativo() {
+        return sSistemaOperativo;
+    }
+
+    public void setsSistemaOperativo(String sSistemaOperativo) {
+        this.sSistemaOperativo = sSistemaOperativo;
+    }
+
+    public String getCaminoCompletoBD() {
+        return CaminoCompletoBD;
+    }
+
+    public void setCaminoCompletoBD(String CaminoCompletoBD) {
+        this.CaminoCompletoBD = CaminoCompletoBD;
+    }
+
+    public boolean isConectado() {
+        return conectado;
+    }
+
+    public void setConectado(boolean conectado) {
+        this.conectado = conectado;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public Boolean getUsuarioValido() {
+        return UsuarioValido;
+    }
+
+    public void setUsuarioValido(Boolean UsuarioValido) {
+        this.UsuarioValido = UsuarioValido;
+    }
+
+    public boolean isEdicion() {
+        return edicion;
+    }
+
+    public void setEdicion(boolean edicion) {
+        this.edicion = edicion;
+    }
+
+    public void cierra() {
         try {
             conectar().close();
         } catch (SQLException ex) {
@@ -53,4 +114,13 @@ public final class conecta {
         }
     }
     
+    public String getCurrentDir() {
+        File currentDirFile = new File(".");
+        String helper = currentDirFile.getAbsolutePath();
+        String dir;
+//        System.out.print(helper);
+        dir = helper.substring(0, helper.length() - 2); //this line may need a try-catch
+        return dir;
+    }
+
 }
